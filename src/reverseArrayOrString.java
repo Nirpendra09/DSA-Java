adee5b932cf8227c634ed57156265b14ea70d6b7
User <55339698+Nirpendra09@users.noreply.github.com>
2021-01-04 16:05:03 +0530

    reverse

--- src/reverseArrayOrString.java
+++ src/reverseArrayOrString.java
@@ -1,5 +1,29 @@
-public class reverseArrayOrString {
-    public static void main(String[] args) {
 
+import java.io.*;
+import java.util.Scanner;
+
+public class reverseArrayOrString {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+
+        char[] ch = input.toCharArray();
+        reverse(ch, 0, input.length() - 1);
+
+        System.out.println(String.valueOf(ch));
+    }
+
+    static void reverse(char arr[], int start, int end) {
+        char temp;
+
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
     }
 }
